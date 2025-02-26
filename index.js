39e2c9470021ded354b2a0b0584e0e395d34a200
FENRiS738 <as420738@gmail.com>
2025-02-26 17:04:40 +0530

    Updated script to extract necessary data of job.

--- index.js
+++ index.js
@@ -8,19 +8,73 @@ app.use(express.json());
 
 puppeteer.use(StealthPlugin());
 
-const get_job_urls = (pageSourceHTML) => {
+const get_job_urls = async (pageSourceHTML) => {
     const JOBS = [];
     const $ = cheerio.load(pageSourceHTML);
 
-    $('.air3-link').each(function () {
-        const jobUrl = $(this).attr('href');
-        if (jobUrl) {
-            JOBS.push("https://www.upwork.com" + jobUrl);
+    $('article.job-tile').each(function () {
+        const jobTitle = $(this).find('h2 a').text().trim();
+        const description = $(this).find('p.text-body-sm').text().trim();
+        const skills = $(this).find('.air3-token').map((i, el) => $(el).text().trim()).get();
+        const jobUrl = "https://www.upwork.com" + $(this).find('h2 a').attr('href');
+        const rateType = $(this).find('ul > [data-test=job-type-label]').text().trim();
+        const experience = $(this).find('ul > [data-test=experience-level]').text().trim();
+        const postedTime = $(this).find('[data-test=job-pubilshed-date] span').map((i, el) => $(el).text().trim()).get().join(" ");
+
+        const job =  {
+            jobTitle,
+            description,
+            skills,
+            jobUrl,
+            rateType,
+            experience,
+            postedTime,
         }
+
+        if(rateType === "Fixed price"){
+            const estBudget = $(this).find('ul > [data-test=is-fixed-price] strong').map((i, el) => $(el).text().trim()).get().join(" ");
+            job['estBudget'] = estBudget;
+        }
+        else{
+            const estTime = $(this).find('ul > [data-test=duration-label] strong').map((i, el) => $(el).text().trim()).get().join(" ");
+            job['estTime'] = estTime;
+        }
+        JOBS.push(job);
     });
 
     return JOBS;
 }
+
+const get_source_html = async (finalUrl) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--disable-features=IsolateOrigins,site-per-process",
+            "--disable-blink-features=AutomationControlled",
+        ],
+    });
+
+    const page = await browser.newPage();
+
+    await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    );
+    await page.setExtraHTTPHeaders({
+        'Accept-Language': 'en-US,en;q=0.9',
+    });
+
+    await page.goto(finalUrl, { waitUntil: 'domcontentloaded' });
+
+    const htmlContent = await page.content();
+    await browser.close();
+
+    return htmlContent;
+}
+
 
 app.get("/", async (req, res) => {
     const baseUrl = "https://www.upwork.com/nx/search/jobs";
@@ -42,39 +96,15 @@ app.get("/", async (req, res) => {
     const finalUrl = searchParams ? `${baseUrl}?${searchParams}` : baseUrl;
 
     try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu",
-                "--disable-features=IsolateOrigins,site-per-process",
-                "--disable-blink-features=AutomationControlled",
-            ],
-        });
+        const pageSourceHTML = await get_source_html(finalUrl);
+        const jobs = await get_job_urls(pageSourceHTML);
 
-        const page = await browser.newPage();
-
-        await page.setUserAgent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-        );
-        await page.setExtraHTTPHeaders({
-            'Accept-Language': 'en-US,en;q=0.9',
-        });
-
-        await page.goto(finalUrl, { waitUntil: "networkidle2" });
-
-        const pageSourceHTML = await page.content();
-        await browser.close();
-
-        const job_urls = get_job_urls(pageSourceHTML);
-
-        res.json({ success: true, message: "Automation completed", job_urls: job_urls });
+        res.json({ success: true, message: "Automation completed" ,jobs });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
